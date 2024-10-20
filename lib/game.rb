b34d3579ba67327c6473614165f341b02e9585e7
harsprad <101750118+harsprad@users.noreply.github.com>
2024-10-20 15:01:03 +0100

    Minor: Class init errors

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,13 @@
 class Game
 
-  def init(word_list_file_name)
-    if File.exist? world_list_file_name
-      File.readlines(world_list_file_name).each do |line|
-        @world_list << line.chomp
+  def initialize(word_list_file_name)
+    @word_list = []
+    if File.exist? word_list_file_name
+      File.readlines(word_list_file_name).each do |line|
+        @word_list << line.chomp
       end
     end
+  end
 
   def play
     puts 'Hangman game started!'
