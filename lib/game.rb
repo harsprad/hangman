54f7f570c18376f0915adb5d05d3530e35ff5b45
harsprad <101750118+harsprad@users.noreply.github.com>
2024-10-20 15:15:59 +0100

    Minor: Put code in fns

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,31 @@
 class Game
 
-  def initialize(word_list_file_name)
-    @word_list = []
-    if File.exist? word_list_file_name
-      File.readlines(word_list_file_name).each do |line|
-        @word_list << line.chomp
-      end
-    end
+  def initialize(list_file)
+    @word_list = file_to_list(list_file)
   end
 
   def play
     puts 'Hangman game started!'
+    puts 'You have 10 attempts to guess all the letters of a hidden word.'
 
+    @secret = @word_list.sample
+
+    # user guess
+    # check if correct
+    # update dislay
+    # if correct win
+    # if no more guesses lose
+    # else loop
   end
+
+  def file_to_list(file)
+    list = []
+    if File.exist? file
+      File.readlines(file).each do |line|
+        list << line.chomp
+      end
+    end
+    list
+  end
+    
 end
