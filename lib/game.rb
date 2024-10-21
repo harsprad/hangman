f10cebb53537d9ef2c4a1c893ca7216ffe898938
harsprad <101750118+harsprad@users.noreply.github.com>
2024-10-21 10:35:08 +0100

    Minor: Guess loop into fn

--- lib/game.rb
+++ lib/game.rb
@@ -2,20 +2,22 @@ class Game
 
   def initialize(list_file)
     @word_list = file_to_list(list_file)
+    @guesses = []
+    @correct_guesses = []
   end
 
   def play
     puts 'Hangman game started!'
-    puts 'You have 10 attempts to guess all the letters of a hidden word.'
+    puts 'You have 10 attempts to guess all the letters of a hidden word.\n'
 
-    @secret = @word_list.sample
+    @secret = @word_list.sample.downcase
 
-    # user guess
-    # check if correct
-    # update dislay
-    # if correct win
-    # if no more guesses lose
-    # else loop
+    guess = get_guess
+
+      # update dislay
+      # if correct win
+      # if no more guesses lose
+      # else loop
   end
 
   def file_to_list(file)
@@ -27,5 +29,22 @@ class Game
     end
     list
   end
-    
+
+  def ask_guess
+    valid_guess = false
+    unless valid_guess
+      puts "Make a new single letter guess:"
+      guess = gets.chomp.downcase
+      valid_guess = true if guess.length == 1
+    end
+    guess
+  end
+
+  def get_guess
+    guess = ""
+    unless @guesses.include?(guess)
+      guess = ask_guess
+    end
+  end
+
 end
